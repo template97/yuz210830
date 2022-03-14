843fbdebdcfa863be1f43c64e79c46e0676fec39
jackson <jackson@classum.com>
2022-03-14 12:11:37 +0900

    feat: Initialize

--- src/pages/game/CreateGamePage.tsx
+++
@@ -1,9 +0,0 @@
-import "./CreateGamePage.scss";
-import React, { useEffect, useState } from "react";
-
-interface Props {}
-const CreateGamePage: React.FC<Props> = (props) => {
-  return <>create game!</>;
-};
-
-export default CreateGamePage;
